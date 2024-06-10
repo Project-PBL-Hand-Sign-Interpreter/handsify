f19266b68194b7dcf6ebd6ca9fbde5dfa9cd5297
Alfian <alfianfakhrudin11@gmail.com>
2024-06-10 12:56:41 +0700

    first commit for frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const videoRef = useRef(null);
+  const [isCameraAvailable, setIsCameraAvailable] = useState(false);
+
+  useEffect(() => {
+    // Mengecek apakah browser mendukung akses ke kamera
+    const checkCameraSupport = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (stream) {
+          setIsCameraAvailable(true);
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        }
+      } catch (error) {
+        console.error('Tidak dapat mengakses kamera:', error);
+      }
+    };
+
+    checkCameraSupport();
+
+    // Membersihkan penggunaan kamera saat komponen dilepas
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const stream = videoRef.current.srcObject;
+        const tracks = stream.getTracks();
+
+        tracks.forEach(track => {
+          track.stop();
+        });
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isCameraAvailable ? (
+        <video
+          ref={videoRef}
+          style={{ width: '100%', maxWidth: '500px', maxHeight: '500px' }}
+          autoPlay
+        ></video>
+      ) : (
+        <p>Kamera tidak tersedia</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
